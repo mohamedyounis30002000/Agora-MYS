3675b2fcfd778a0b3f666200fe7a10adabbfd229
mohamedyounis30002000 <61964822+mohamedyounis30002000@users.noreply.github.com>
2024-01-23 12:52:10 +0200

    index.js

--- index.js
+++ index.js
@@ -6,17 +6,20 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/generateToken', (req, res) => {
-    const appID = '11efb81da2e0405cb0f4b1c159b66e0d';
-    const appCertificate = '76bf6543723d4963a0d33c2a25999da2';
-    //const channelName = req.query.channelName;
+    //const appID = '11efb81da2e0405cb0f4b1c159b66e0d';
+    const appID = req.query.appID;
+    //const appCertificate = '76bf6543723d4963a0d33c2a25999da2';
+    const appCertificate = req.query.appCertificate;
+    const channelName = req.query.channelName;
     //const uid = req.query.uid;
-    const channelName = "myse";
-    const uid = 2882341273;
-    const account = "2882341273";
-    const role = RtcRole.PUBLISHER ; // Use RtcRole.SUBSCRIBER for audience role //PUBLISHER
+    //const uid = 2882341273;
+    const uid = req.query.appUid;
+    //const account = "2882341273";
+    const account = req.query.appUid;
+    const role = RtcRole.PUBLISHER ; // SUBSCRIBER //PUBLISHER
 
-    const expirationTimeInSeconds = 3600 *24 ;
-
+    const expirationTimeInSeconds = 3600 *24;
+    
     const key = RtcTokenBuilder.buildTokenWithUid(
         appID,
         appCertificate,
